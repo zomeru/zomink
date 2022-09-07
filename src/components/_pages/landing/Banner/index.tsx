937a2f67a1e08bb705234c740910dc2b1d7dd47e
Zomer <zomerzxc.19@gmail.com>
2022-09-08 03:07:31 +0800

    tweak cross sign

--- src/components/_pages/landing/Banner/index.tsx
+++ src/components/_pages/landing/Banner/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Banner = () => {
+  const { push } = useRouter();
+
   return (
     <section className='padding-sides my-[80px] bg-primary-200'>
       <div className='max-width space-y-5 py-[50px]'>
@@ -8,7 +11,11 @@ const Banner = () => {
           More than just a free link shortener
         </h2>
         <div className='flex justify-center'>
-          <button type='button' className='btn-white-lg'>
+          <button
+            type='button'
+            className='btn-white-lg'
+            onClick={() => push('/auth/register')}
+          >
             Sign up for free
           </button>
         </div>
